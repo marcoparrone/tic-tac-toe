8cce50f102b4b4db851349337c0b7f528dcbfc6c
Marco Parrone <marco@marcoparrone.com>
2020-12-21 18:39:56 +0100

    Back to gh-pages.

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,7 +10,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 registerRoute(
   /.*marcoparrone\.github\.io/,
   new StaleWhileRevalidate({
-    cacheName: 'marcoparrone',
+    cacheName: 'marcoparronegh',
   })
 );
 
